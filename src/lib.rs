6937fe39af6e292915d183e1c89c45f6a1e764da
Mateusz Lajszczak <mlajszczak@gmail.com>
2017-06-10 13:19:15 +0200

    Test Ty::compose() better

--- src/lib.rs
+++ src/lib.rs
@@ -23,14 +23,14 @@ impl Ty {
     pub fn bool() -> Rc<Ty> {
         Rc::new(Ty::Bool)
     }
-    
+
     pub fn nat() -> Rc<Ty> {
         Rc::new(Ty::Nat)
     }
 
     pub fn compose(s1: &Subs, s2: &Subs) -> Subs {
         let mut composed = HashMap::new();
-    
+
         for (var, ty) in s1 {
             let applied = Ty::apply(ty, &s2);
             match *applied {
@@ -43,13 +43,13 @@ impl Ty {
             }
             composed.insert(*var, applied);
         }
-    
+
         for (var, ty) in s2 {
             if !s1.contains_key(var) {
                 composed.insert(*var, ty.clone());
             }
         }
-    
+
         composed
     }
 
@@ -143,9 +143,9 @@ mod tests {
             s.insert(2, Ty::nat());
 
             let applied = Ty::arr(
-                Ty::arr(Ty::var(0), Ty::bool()), 
+                Ty::arr(Ty::var(0), Ty::bool()),
                 Ty::arr(
-                    Ty::nat(), 
+                    Ty::nat(),
                     Ty::arr(Ty::nat(), Ty::arr(Ty::var(2), Ty::var(1)))));
 
             assert_eq!(Ty::apply(&ty, &s), applied);
@@ -156,6 +156,15 @@ mod tests {
         use super::*;
 
         #[test]
+        fn inverting_subs() {
+            let mut s = HashMap::new();
+            s.insert(0, Ty::var(1));
+            s.insert(1, Ty::var(0));
+
+            assert_eq!(Ty::compose(&s, &s), HashMap::new());
+        }
+
+        #[test]
         fn neutralize_fist_subs() {
             let mut s1 = HashMap::new();
             s1.insert(0, Ty::var(1));
@@ -164,6 +173,45 @@ mod tests {
             s2.insert(1, Ty::var(0));
 
             assert_eq!(Ty::compose(&s1, &s2), s2);
+        }
+
+        #[test]
+        fn no_common_variables() {
+            let mut s1 = HashMap::new();
+            s1.insert(0, Ty::nat());
+            s1.insert(1, Ty::var(0));
+
+            let mut s2 = HashMap::new();
+            s2.insert(2, Ty::var(1));
+            s2.insert(3, Ty::bool());
+
+            let mut composed = HashMap::new();
+            composed.insert(0, Ty::nat());
+            composed.insert(1, Ty::var(0));
+            composed.insert(2, Ty::var(1));
+            composed.insert(3, Ty::bool());
+
+            assert_eq!(Ty::compose(&s1, &s2), composed);
+        }
+
+        #[test]
+        fn complex_composition() {
+            let mut s1 = HashMap::new();
+            s1.insert(0, Ty::arr(Ty::arr(Ty::var(2), Ty::bool()), Ty::var(1)));
+            s1.insert(1, Ty::nat());
+
+            let mut s2 = HashMap::new();
+            s2.insert(1, Ty::arr(Ty::var(2), Ty::bool()));
+            s2.insert(2, Ty::arr(Ty::nat(), Ty::var(1)));
+
+            let mut composed = HashMap::new();
+            composed.insert(0, Ty::arr(
+                    Ty::arr(Ty::arr(Ty::nat(), Ty::var(1)), Ty::bool()),
+                    Ty::arr(Ty::var(2), Ty::bool())));
+            composed.insert(1, Ty::nat());
+            composed.insert(2, Ty::arr(Ty::nat(), Ty::var(1)));
+
+            assert_eq!(Ty::compose(&s1, &s2), composed);
         }
     }
 }
