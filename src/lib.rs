688d6d90aa5d120c467f47fc9ff819cd9ede2b5a
Mateusz Lajszczak <mlajszczak@gmail.com>
2017-06-10 07:00:06 +0200

    Define Subs type
    Subs represents type substitution. It's an alias for HashMap type with
    appropriate type arguments.
    
    While at it, send a reference to compose() instead of moving.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ use std::collections::HashMap;
 use std::collections::HashSet;
 use std::rc::Rc;
 
+pub type Subs = HashMap<u32, Rc<Ty>>;
+
 #[derive(Debug, PartialEq)]
 pub enum Ty {
     Var(u32),
@@ -27,15 +29,15 @@ impl Ty {
         Rc::new(Ty::Nat)
     }
 
-    pub fn compose(s1: HashMap<u32, Rc<Ty>>, s2: HashMap<u32, Rc<Ty>>) -> HashMap<u32, Rc<Ty>> {
+    pub fn compose(s1: &Subs, s2: &Subs) -> Subs {
         let mut composed = HashMap::new();
         let s1_keys = s1.keys().collect::<HashSet<&u32>>();
     
-        for (var, ty) in &s1 {
+        for (var, ty) in s1 {
             composed.insert(*var, Ty::apply(ty, &s2));
         }
     
-        for (var, ty) in &s2 {
+        for (var, ty) in s2 {
             if !s1_keys.contains(var) {
                 composed.insert(*var, ty.clone());
             }
@@ -44,7 +46,7 @@ impl Ty {
         composed
     }
 
-    pub fn apply(ty: &Rc<Ty>, s: &HashMap<u32, Rc<Ty>>) -> Rc<Ty> {
+    pub fn apply(ty: &Rc<Ty>, s: &Subs) -> Rc<Ty> {
         let s_keys = s.keys().collect::<HashSet<&u32>>();
     
         match apply_rec(ty, s, &s_keys) {
@@ -54,7 +56,7 @@ impl Ty {
     }
 }
 
-fn apply_rec(ty: &Rc<Ty>, s: &HashMap<u32, Rc<Ty>>, s_keys: &HashSet<&u32>) -> Option<Rc<Ty>> {
+fn apply_rec(ty: &Rc<Ty>, s: &Subs, s_keys: &HashSet<&u32>) -> Option<Rc<Ty>> {
     match **ty {
         Ty::Var(ref var) => {
             if s_keys.contains(&var) {
